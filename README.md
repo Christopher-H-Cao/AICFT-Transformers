f0c9cfb29602fb867707a75b2c37daee318277da
Garrett Merz <garrettwmerz@gmail.com>
2024-08-02 11:38:10 -0500

    cleanup README

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ Shuffle the file. `make_cft_data_from_csv.py` outputs this line for you to copy)
 
 Build train, validation and test set. First parameter is the name of the file to split (produced at the previous step), second is the number of elements in the train and test set .
 We end up with three files for each dataset, that will be used by parameter `reload_data`of the transformer code.
+"--no_test" only generates two files, in case we do not want to do hyperparameter tuning (this is a decent option for experiments).
 
 ## Running the model
 
@@ -45,3 +46,31 @@ All package dependencies are listed in requirements.txt. The relatively-small do
 
 # Tensorboard
 Metrics are logged on wandb- create a weights and biases account to follow trainings as they happen. If no wandb account is found, logging will happen on tensorboard instead.
+
+
+#to run on CHTC:
+To run this on CHTC on a new dataset:
+
+First, process the data into the desired input-output format using "make_cft_data_from_csv.py".
+Shuffle the output file and split it into a file.train and a file.valid (and possibly a file.test as well).
+For early experiments, I recommend a 90%-10% train/valid split with no test.
+Put these files into a directory. I recommend keeping this outside this repo, in case files get large:
+accidentally checking large files into git can cause major problems!
+
+
+To run, make a tarball containing the repository folder and your data folder:
+`tar --exclude-vcs -czvf runcft_package.tar ChromoBoot_CFT {YOUR_DATA_FOLDER}`
+IMPORTANT: If you've run the model locally, make sure to delete the "wandb" and "checkpoint" directories before doing this!
+
+Copy the tarball to the transfer node on CHTC. I use 'staging' for this and recommend you do as well.
+
+Edit the .sub and .sh scripts as needed. Add your WANDB_API key.
+Make sure that the .sh script unpacks the tarball, cds into the ChromoBoot_CFT directory, copies the data into it if it is not there already,
+and that the train.py command points to the place the data is located in the ChromoBoot_CFT directory.
+
+Copy the .sub and .sh scripts to the submit node on CHTC.
+
+Edit them as needed- make sure the .sub script correctly points to the .sh one.
+do: `condor_submit run_training_cfts.sub`
+
+Wait
--- run_training_cfts.sh
+++ run_training_cfts.sh
@@ -3,7 +3,7 @@ echo "Hello CHTC from Job running on `hostname`"
 echo "training- cft cc prediction"
 
 # This is secret and shouldn't be checked into version control
-export WANDB_API_KEY="334225e5e6323c8ba4460abb984e0ae6c7590a12"
+export WANDB_API_KEY={YOUR_API_KEY}
 # Name and notes optional
 export WANDB_NAME=""
 export WANDB_NOTES="ChromoBoot_CFT"
@@ -15,8 +15,10 @@ export WANDB_PROJECT="ChromoBoot_CFT"
 #your tarball. Should contain ChromoBoot_CFT code and sample_data.
 #Feel free to restructure as needed, but by the time the train command is passed,
 #everything should be in a subdirectory of ChromoBoot_CFT.
-cp -r your_tarball.tar .
-tar -xzvf your_tarball.tar
+cp -r runcft_package.tar .
+tar -xzvf runcft_package.tar
+
+# This block is for the exit-driven checkpointing. Leave it alone!
 # Check if checkpoint tarball exists; if so then unpack
 checkpoint_tarball=checkpoint_${4}.tar.gz
 if [[ -f "$checkpoint_tarball" ]]; then
@@ -27,10 +29,15 @@ mv ./checkpoint ChromoBoot_CFT
 mv ./runs_train ChromoBoot_CFT
 mv ./wandb ChromoBoot_CFT
 fi
+
+#change to run directory to actually run the trainiing
 cd ChromoBoot_CFT
+#move your data from the directory it lives in to the run directory, if it's not there already.
 mkdir cft_data
-cp -r ../cft_data/su2_cfts.* ./cft_data
-timeout 4h python train.py --reload_data cfts,./cft_data/su2_cfts.data.train,./cft_data/su2_cfts.data.valid,./cft_data/su2_cfts.data.valid --max_epoch 20 --n_enc_layers $2 --n_dec_layers $2 --num_workers 1 --eval_verbose 1 --batch_size 512 --batch_size_eval 10 --enc_emb_dim 512 --dec_emb_dim 512 --amp 1 --fp16 True --epoch_size 900  --resume_wandb True --env_base_seed $3 --exp_id $4 
+cp -r {PATH TO YOUR DATA} ./cft_data
+
+
+timeout 4h python train.py --reload_data cfts,./cft_data/{DATA_FILE}.train,./cft_data/{DATA_FILE}.valid,./cft_data/{DATA_FILE}.valid --max_epoch 20 --n_enc_layers $2 --n_dec_layers $2 --num_workers 1 --eval_verbose 1 --batch_size 512 --batch_size_eval 10 --enc_emb_dim 512 --dec_emb_dim 512 --amp 1 --fp16 True --epoch_size 900  --resume_wandb True --env_base_seed $3 --exp_id $4 
 timeout_exit_status=$?
 # Uses the bash notation of `$?` to call the exit value of the last executed command
 # and to save it in a variable called `timeout_exit_status`.
